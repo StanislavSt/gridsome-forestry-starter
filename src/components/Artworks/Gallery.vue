9e83428802892657276fd16f76dd9c7af09cfc11
StanislavSt <s.staev1@gmail.com>
2019-07-06 00:22:18 +0200

    eslint

--- src/components/Artworks/Gallery.vue
+++ src/components/Artworks/Gallery.vue
@@ -1,137 +1,126 @@
 <template>
   <div>
     <div class="category">
-      <a v-on:click="filter('canvas')" class="btn btn-dark" >Canvas</a>
-      <a v-on:click="filter('reproduction')" class="btn btn-dark" >Reproduction</a>
-      <a v-on:click="filter('print')" class="btn btn-dark" >Print</a>
+      <a v-on:click="filter('canvas')" class="btn btn-dark">Canvas</a>
+      <a v-on:click="filter('reproduction')" class="btn btn-dark">Reproduction</a>
+      <a v-on:click="filter('print')" class="btn btn-dark">Print</a>
     </div>
     <div class="projects">
-        <div class="project" v-for="item in filtered(this.category)" :key="item.node.id">
-          <!-- Sold artworks-->
-          <div class="sold" v-if="item.node.categories.includes('sold') && ((category == 'all') || (category == 'canvas')) && !item.node.categories.includes('reproduction')">
-            <div class="image-text">
-              <g-link :to="item.node.path" class="project-link">
-              <g-image
-                  :src="item.node.thumbnail"
-                  :alt="item.node.title"
-                  class="thumbnail"
-              />
+      <div class="project" v-for="item in filtered(this.category)" :key="item.node.id">
+        <!-- Sold artworks-->
+        <div
+          class="sold"
+          v-if="item.node.categories.includes('sold') && ((category == 'all') || (category == 'canvas')) && !item.node.categories.includes('reproduction')"
+        >
+          <div class="image-text">
+            <g-link :to="item.node.path" class="project-link">
+              <g-image :src="item.node.thumbnail" :alt="item.node.title" class="thumbnail" />
               <div class="centered">SOLD</div>
-              </g-link>
-              </div>
-            </div>
-            <!-- Prints -->
-            <div class="print" v-else-if="category =='print'">
-              <g-link :to="item.node.path" class="project-link">
-              <g-image
-                  :src="item.node.thumbnail"
-                  :alt="item.node.title"
-                  class="thumbnail"
-              />
-              </g-link>
-            </div>
-            <!-- In stock artworks-->
-            <div class="instock" v-else>
-              <g-link :to="item.node.path" class="project-link">
-              <g-image
-                  :src="item.node.thumbnail"
-                  :alt="item.node.title"
-                  class="thumbnail"
-              />
-              </g-link>
-            </div>
+            </g-link>
+          </div>
         </div>
+        <!-- Prints -->
+        <div class="print" v-else-if="category =='print'">
+          <g-link :to="item.node.path" class="project-link">
+            <g-image :src="item.node.thumbnail" :alt="item.node.title" class="thumbnail" />
+          </g-link>
+        </div>
+        <!-- In stock artworks-->
+        <div class="instock" v-else>
+          <g-link :to="item.node.path" class="project-link">
+            <g-image :src="item.node.thumbnail" :alt="item.node.title" class="thumbnail" />
+          </g-link>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-    props: {
-        projects: {
-            type: Array,
-            required: true
+  props: {
+    projects: {
+      type: Array,
+      required: true
+    }
+  },
+  data: function() {
+    return {
+      category: "all"
+    };
+  },
+  methods: {
+    filtered(id) {
+      return this.projects.filter(function(value) {
+        if (id == "all") {
+          return value;
+        } else {
+          return value.node.categories.includes(id);
         }
+      });
     },
-    data: function(){
-      return {
-        category: 'all'
-        }
-    },
-    methods: {
-       filtered(id){
-        return this.projects.filter(function(value){
-          if(id == 'all')
-          {
-            return value;
-          }
-          else{
-            return value.node.categories.includes(id);
-          }
-        })
-      },
-      filter: function(tag) {
+    filter: function(tag) {
       this.category = tag;
     }
   }
-}
+};
 </script>
 
 <style scoped>
-@import url('https://fonts.googleapis.com/css?family=Karla&display=swap');
+@import url("https://fonts.googleapis.com/css?family=Karla&display=swap");
 /*------------------------------------------------------*/
 /* BUTTONS */
 /*------------------------------------------------------*/
 .btn,
-input[type="submit"]{
-	cursor: pointer;
-    border-radius: 0px;
-    padding: 12px 18px;
-    font-size: 12px;
-    line-height: 19px;
-    text-transform: uppercase;
-    font-family: 'Karla', sans-serif;
-    font-weight: bold;
-    letter-spacing: 1px;
-    -webkit-transition: all .4s ease-in-out;
-       -moz-transition: all .4s ease-in-out;
-        -ms-transition: all .4s ease-in-out;
-         -o-transition: all .4s ease-in-out;
-            transition: all .4s ease-in-out;
+input[type="submit"] {
+  cursor: pointer;
+  border-radius: 0px;
+  padding: 12px 18px;
+  font-size: 12px;
+  line-height: 19px;
+  text-transform: uppercase;
+  font-family: "Karla", sans-serif;
+  font-weight: bold;
+  letter-spacing: 1px;
+  -webkit-transition: all 0.4s ease-in-out;
+  -moz-transition: all 0.4s ease-in-out;
+  -ms-transition: all 0.4s ease-in-out;
+  -o-transition: all 0.4s ease-in-out;
+  transition: all 0.4s ease-in-out;
 }
 .btn:hover,
-input[type="submit"]:hover{
-    -webkit-transition: all .4s ease-in-out;
-       -moz-transition: all .4s ease-in-out;
-        -ms-transition: all .4s ease-in-out;
-         -o-transition: all .4s ease-in-out;
-            transition: all .4s ease-in-out;
+input[type="submit"]:hover {
+  -webkit-transition: all 0.4s ease-in-out;
+  -moz-transition: all 0.4s ease-in-out;
+  -ms-transition: all 0.4s ease-in-out;
+  -o-transition: all 0.4s ease-in-out;
+  transition: all 0.4s ease-in-out;
 }
 .btn-dark,
-input[type="submit"]{
-    background: transparent;
-    color: #1f1f1f;
-    padding-right: 2rem;
-    padding-left: 2rem;
+input[type="submit"] {
+  background: transparent;
+  color: #1f1f1f;
+  padding-right: 2rem;
+  padding-left: 2rem;
 }
 .btn-dark:hover,
 input[type="submit"]:hover,
-.btn-dark.active{
-    font-size: 13px;
-    background: 	#E8E8E8;
+.btn-dark.active {
+  font-size: 13px;
+  background: #e8e8e8;
 }
 
-.btn-color{
-    background: transparent;
+.btn-color {
+  background: transparent;
 }
-.btn-color:hover{
-    color: #fff;
+.btn-color:hover {
+  color: #fff;
 }
 
 .category {
-    text-align: center;
-    max-width: 100%;
-    padding: 0rem 0 3rem 0rem;
+  text-align: center;
+  max-width: 100%;
+  padding: 0rem 0 3rem 0rem;
 }
 .projects {
   display: grid;
@@ -149,7 +138,7 @@ input[type="submit"]:hover,
   height: 190px;
   object-fit: cover;
   transition: all 0.15s ease;
-  box-shadow: 0 0 40px -20px rgba(0,0,0,0.25);
+  box-shadow: 0 0 40px -20px rgba(0, 0, 0, 0.25);
 }
 .project-title {
   font-size: 0.9rem;
@@ -165,7 +154,7 @@ input[type="submit"]:hover,
 }
 .project:hover .thumbnail {
   transform: scale(1.02);
-  box-shadow: 0 20px 40px -20px rgba(0,0,0,0.25);
+  box-shadow: 0 20px 40px -20px rgba(0, 0, 0, 0.25);
 }
 .centered {
   position: absolute;
@@ -174,26 +163,24 @@ input[type="submit"]:hover,
   transform: translate(-50%, -50%);
   opacity: 1;
 }
-.image-text{
+.image-text {
   position: relative;
   text-align: center;
   color: white;
-  opacity:0.8;
+  opacity: 0.8;
 }
 @media (min-width: 920px) {
   .project {
     grid-column: auto / span 1;
   }
-  .project:nth-child(3n+1) {
-    grid-column: auto ;
+  .project:nth-child(3n + 1) {
+    grid-column: auto;
   }
 }
 @media only screen and (max-width: 768px) {
-  .category{
-  display:grid;
-  grid-template-columns: 1fr;
+  .category {
+    display: grid;
+    grid-template-columns: 1fr;
   }
 }
-
-
 </style>
--- src/components/Home/Carousel.vue
+++ src/components/Home/Carousel.vue
@@ -1,63 +1,59 @@
 <template>
-<ClientOnly>
-  <div class="container-carousel">
-    <carousel :per-page="1" :mouse-drag="false" :autoplay="true" 
-    :centerMode="true" :autoplayTimeout="3000">
-      <slide class="example-slide">
-        <div class="bottom-right">2018 Solo exhibition "Debut", gallery "Bulgari", Sofia, Bulgaria</div>
-        <img 
-            src="../../../static/Thea_About.jpg"
-        />
-      </slide>
-      <slide class="example-slide">
-        <img 
-            src="../../../static/Thea_Contact.jpg"
-        />
-      </slide>
-    </carousel>
-  </div>
-</ClientOnly>
+  <ClientOnly>
+    <div class="container-carousel">
+      <carousel
+        :per-page="1"
+        :mouse-drag="false"
+        :autoplay="true"
+        :centerMode="true"
+        :autoplayTimeout="3000"
+      >
+        <slide class="example-slide">
+          <div class="bottom-right">2018 Solo exhibition "Debut", gallery "Bulgari", Sofia, Bulgaria</div>
+          <img src="../../../static/Thea_About.jpg" />
+        </slide>
+        <slide class="example-slide">
+          <img src="../../../static/Thea_Contact.jpg" />
+        </slide>
+      </carousel>
+    </div>
+  </ClientOnly>
 </template>
 
 <script>
 export default {
   data() {
-    return {
-    }
+    return {};
   },
   components: {
-      Carousel: () =>
-        import ('vue-carousel')
+    Carousel: () =>
+      import("vue-carousel")
         .then(m => m.Carousel)
         .catch(),
-      Slide: () =>
-        import ('vue-carousel')
+    Slide: () =>
+      import("vue-carousel")
         .then(m => m.Slide)
         .catch()
-    },
-}
+  }
+};
 </script>
 
-
 <style scoped>
-.example-slide
-{
-  height:30rem;
+.example-slide {
+  height: 30rem;
 }
 .bottom-right {
   position: absolute;
   bottom: 8px;
   right: 16px;
-  color:white;
+  color: white;
 }
-.container-carousel{
+.container-carousel {
   padding-bottom: 3rem;
 }
 @media only screen and (max-width: 768px) {
-  .example-slide
-  {
+  .example-slide {
     height: 15rem;
   }
 }
-
 </style>
