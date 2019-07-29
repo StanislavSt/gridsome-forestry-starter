33af1f12d1f7110383b58f2225cfdfaf60e8164e
StanislavSt <s.staev1@gmail.com>
2019-07-29 21:09:46 +0200

    Added terms and condition page

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,6 +2,7 @@
   <footer class="footer">
     <div class="container">
       <span class="copyright" v-html="settings.footer" />
+      <g-link class="nav__link" :to="{ name: 'terms' }">Terms and Conditions</g-link>
     </div>
   </footer>
 </template>
@@ -25,9 +26,18 @@ export default {
   border-top: groove;
   padding-top: 2rem;
 }
-@media only screen and (max-width: 345px) {
+.nav__link{
+  font-size: 14px;
+  float:right;
+}
+@media only screen and (max-width: 390px) {
   .copyright {
     font-size: 0.6rem;
   }
+  .nav__link{
+  padding-top:3px;
+  font-size: 11px;
+  float:right;
+}
 }
 </style>
---
+++ src/pages/Terms.vue
@@ -0,0 +1,19 @@
+<template>
+  <layout>
+    <div class="container">
+      <TermsContent/>
+    </div>
+  </layout>
+</template>
+<script>
+import TermsContent from "@/components/Terms/TermsContent.vue";
+export default {
+  components: {
+    TermsContent
+  }
+};
+</script>
+
+<style>
+</style>
+
